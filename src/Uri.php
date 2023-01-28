5caf75c30398c06acae44732c1a3270457416f53
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-27 23:14:02 -0300

    criação da classe Uri

---
+++ src/Uri.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Minizord\Http;
+
+use Minizord\Http\Contract\UriInterface;
+
+class Uri implements UriInterface
+{
+    public function __construct(string $url)
+    {
+    }
+
+    public function getScheme()
+    {
+    }
+
+    public function getAuthority()
+    {
+    }
+
+    public function getUserInfo()
+    {
+    }
+
+    public function getHost()
+    {
+    }
+
+    public function getPort()
+    {
+    }
+
+    public function getPath()
+    {
+    }
+
+    public function getQuery()
+    {
+    }
+
+    public function getFragment()
+    {
+    }
+
+    public function withScheme($scheme)
+    {
+    }
+
+    public function withUserInfo($user, $password = null)
+    {
+    }
+
+    public function withHost($host)
+    {
+    }
+
+    public function withPort($port)
+    {
+    }
+
+    public function withPath($path)
+    {
+    }
+
+    public function withQuery($query)
+    {
+    }
+
+    public function withFragment($fragment)
+    {
+    }
+
+    public function __toString()
+    {
+    }
+}
