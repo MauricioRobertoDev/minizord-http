fb02bc76e082c7b7073b9ac79a0aca1a945036b6
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-01-30 11:10:15 -0300

    documentação

--- src/MessageTrait.php
+++ src/MessageTrait.php
@@ -7,16 +7,47 @@ use Psr\Http\Message\StreamInterface as PsrStreamInterface;
 
 trait MessageTrait
 {
+    /**
+     * Versão do protocolo.
+     *
+     * @var string
+     */
     protected string $protocol   = '1.1';
+    /**
+     * Headers e seus valores.
+     *
+     * @var array
+     */
     protected array $headers     = [];
+    /**
+     * Nome do header normalizado e o seu original.
+     *
+     * @var array
+     */
     protected array $headerNames = [];
-    protected PsrStreamInterface $body;
+    /**
+     * Uma stream.
+     *
+     * @var PsrStreamInterface
+     */
+    protected ?PsrStreamInterface $body = null;
 
+    /**
+     * Retorna o a versão do protoloco ex. 1.1 1.0.
+     *
+     * @return string
+     */
     public function getProtocolVersion() : string
     {
         return $this->protocol;
     }
 
+    /**
+     * Retorna uma nova instância com a versão do protocolo passado.
+     *
+     * @param  string $version
+     * @return self
+     */
     public function withProtocolVersion($version) : self
     {
         $this->validateProtocolVersion($version);
@@ -26,16 +57,33 @@ trait MessageTrait
         return $clone;
     }
 
+    /**
+     * Retornar os headers.
+     *
+     * @return array
+     */
     public function getHeaders() : array
     {
         return $this->headers;
     }
 
+    /**
+     * Checa se um header existe ou não.
+     *
+     * @param  string $name
+     * @return bool
+     */
     public function hasHeader($name) : bool
     {
         return $this->getOriginalHeaderName($name) ?? false;
     }
 
+    /**
+     * Pega os valores de determinado header.
+     *
+     * @param  string $name
+     * @return array
+     */
     public function getHeader($name) : array
     {
         if ($this->hasHeader($name)) {
@@ -45,11 +93,24 @@ trait MessageTrait
         return [];
     }
 
+    /**
+     * Retorna os valores de determinado header em uma string.
+     *
+     * @param  string $name
+     * @return string
+     */
     public function getHeaderLine($name) : string
     {
         return join(', ', $this->getHeader($name));
     }
 
+    /**
+     * Retorna uma nova instância com o header passado.
+     *
+     * @param  string          $name
+     * @param  string|string[] $value
+     * @return self
+     */
     public function withHeader($name, $value) : self
     {
         if (!is_string($name)) {
@@ -70,6 +131,13 @@ trait MessageTrait
         return $clone;
     }
 
+    /**
+     * Retorna uma nova instância com os valores passados adicionado ao header existente.
+     *
+     * @param  string          $name
+     * @param  string|string[] $value
+     * @return self
+     */
     public function withAddedHeader($name, $value) : self
     {
         if (!is_string($name)) {
@@ -90,6 +158,12 @@ trait MessageTrait
         return $clone;
     }
 
+    /**
+     * Retorna uma nova instância sem o header passado.
+     *
+     * @param  string $name
+     * @return self
+     */
     public function withoutHeader($name) : self
     {
         $clone = clone $this;
@@ -98,6 +172,11 @@ trait MessageTrait
         return $clone;
     }
 
+    /**
+     * Retorna a stream atual, ou uma nova caso não exista.
+     *
+     * @return PsrStreamInterface
+     */
     public function getBody() : PsrStreamInterface
     {
         if (!isset($this->stream)) {
@@ -107,6 +186,12 @@ trait MessageTrait
         return $this->stream;
     }
 
+    /**
+     * Retorna uma nova instância com a stream passada.
+     *
+     * @param  PsrStreamInterface $body
+     * @return self
+     */
     public function withBody(PsrStreamInterface $body) : self
     {
         $clone         = clone $this;
@@ -116,6 +201,13 @@ trait MessageTrait
     }
 
     // private
+
+    /**
+     * Valida a versão do protocolo.
+     *
+     * @param  string $version
+     * @return void
+     */
     private function validateProtocolVersion($version) : void
     {
         if (empty($version)) {
@@ -127,6 +219,12 @@ trait MessageTrait
         }
     }
 
+    /**
+     * Valida o nome do header.
+     *
+     * @param  string $name
+     * @return string
+     */
     private function filterHeaderName(string $name) : string
     {
         $name = trim((string) $name, " \t");
@@ -138,6 +236,12 @@ trait MessageTrait
         return $name;
     }
 
+    /**
+     * Valida o valor do header.
+     *
+     * @param  string|array $value
+     * @return array
+     */
     private function filterHeaderValue(string|array $value) : array
     {
         if (is_string($value)) {
@@ -154,6 +258,12 @@ trait MessageTrait
         return $value;
     }
 
+    /**
+     * Retorna o nome original do header.
+     *
+     * @param  string $name
+     * @return void
+     */
     private function getOriginalHeaderName(string $name) : string | null
     {
         return $this->headerNames[strtolower($name)] ?? null;
