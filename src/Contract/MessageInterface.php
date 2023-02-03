e321f94250df84f0733b6b951bccc649c978401b
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-02-03 15:11:28 -0300

    adicionado alguns métodos próprios desta implementação

--- src/Contract/MessageInterface.php
+++ src/Contract/MessageInterface.php
@@ -6,4 +6,7 @@ use Psr\Http\Message\MessageInterface as PsrMessageInterface;
 
 interface MessageInterface extends PsrMessageInterface
 {
+    public function inHeader(string $name, string|array $values) : bool;
+
+    public function inHeaderAny(string $name, array $values) : bool;
 }
