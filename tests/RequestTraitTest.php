24cc09aa67130c07ae6d08ffaff7ac72e2d74c48
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-02-03 18:39:23 -0300

    pequenas alterações e documentação

--- tests/RequestTraitTest.php
+++ tests/RequestTraitTest.php
@@ -3,6 +3,9 @@
 use Minizord\Http\RequestTrait;
 use Minizord\Http\Uri;
 
+/*
+ * withMethod()
+ */
 test('Deve uma nova instância com o método passado', function () {
     $request = new class() {
         use RequestTrait;
@@ -10,39 +13,90 @@ test('Deve uma nova instância com o método passado', function () {
 
     expect($request->withMethod('post')->getMethod())->toBe('post');
     expect($request->withMethod('pOst')->getMethod())->toBe('pOst');
+});
+
+test('Deve estourar uma erro caso passe um método inválido', function () {
+    $request = new class() {
+        use RequestTrait;
+    };
+
     expect(fn () => $request->withMethod(888)->getMethod())->toThrow(InvalidArgumentException::class);
     expect(fn () => $request->withMethod(null)->getMethod())->toThrow(InvalidArgumentException::class);
     expect(fn () => $request->withMethod(' ')->getMethod())->toThrow(InvalidArgumentException::class);
     expect(fn () => $request->withMethod('@')->getMethod())->toThrow(InvalidArgumentException::class);
 });
 
+/*
+ * withRequestTarget()
+ */
 test('Deve uma nova instância com o request target passado', function () {
     $request = new class() {
         use RequestTrait;
     };
 
     expect($request->withRequestTarget('/batata')->getRequestTarget())->toBe('/batata');
-    expect(fn () =>$request->withRequestTarget('/path/ path'))->toThrow(InvalidArgumentException::class);
+    expect($request->withRequestTarget('/batata/?arg=value')->getRequestTarget())->toBe('/batata/?arg=value');
+    expect($request->withRequestTarget('/batata?arg=value')->getRequestTarget())->toBe('/batata?arg=value');
 });
 
-test('Deve uma nova instância com o a uri passada', function () {
+test('Deve estourar um erro caso passe um target inválido', function () {
     $request = new class() {
         use RequestTrait;
     };
 
-    $uri = new Uri('https://example.com');
-    expect($request->withUri($uri)->getUri())->toBe($uri);
+    expect(fn () =>$request->withRequestTarget('/path/ path'))->toThrow(InvalidArgumentException::class);
+    expect(fn () =>$request->withRequestTarget(null))->toThrow(InvalidArgumentException::class);
+    expect(fn () =>$request->withRequestTarget(888))->toThrow(InvalidArgumentException::class);
+});
 
+/*
+ * withUri()
+ */
+test('Deve uma nova instância com a uri passada e setando o host da uri na request', function () {
+    $request = new class() {
+        use RequestTrait;
+    };
     $request = $request->withHeader('host', 'batatinha.com');
+
+    $uri = new Uri('https://example.com');
+    $uri = $uri->withHost('tomatinho.com');
+
+    expect($request->withUri($uri)->getUri())->toBe($uri);
+    expect($request->withUri($uri)->getHeader('host'))->toBe(['tomatinho.com']);
+});
+
+test('Deve retornar uma nova instância com a nova uri e preservando o host atual da request', function () {
+    $request = new class() {
+        use RequestTrait;
+    };
+
+    $uri     = new Uri('https://example.com');
+    $request = $request->withHeader('host', 'batatinha.com');
+
     expect($request->withUri($uri, true)->getUri())->toBe($uri);
     expect($request->withUri($uri, true)->getHeader('host'))->toBe(['batatinha.com']);
+});
 
-    $request = $request->withoutHeader('host');
+test('Deve retornar uma nova instância com a nova uri e sem nenhum host pois nenhum tem', function () {
+    $request = new class() {
+        use RequestTrait;
+    };
+
+    $uri     = new Uri('https://example.com');
     $uri     = $uri->withHost('');
+
     expect($request->withUri($uri, true)->getUri())->toBe($uri);
     expect($request->withUri($uri, true)->getHeader('host'))->toBe([]);
+});
 
-    $uri = $uri->withHost('batata.com')->withPort(8888);
+test('Deve retornar uma nova instância com a nova uri com host da api já que a request não tem', function () {
+    $request = new class() {
+        use RequestTrait;
+    };
+
+    $uri     = new Uri('https://example.com');
+    $uri     = $uri->withHost('batata.com')->withPort(8888);
+
     expect($request->withUri($uri, true)->getHeader('host'))->toBe(['batata.com:8888']);
     expect($request->withUri($uri, false)->getHeader('host'))->toBe(['batata.com:8888']);
 });
